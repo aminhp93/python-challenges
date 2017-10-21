bcb34ac9170713cbe8ee319b480baa7a4fc254c1
Minh Pham <minhpham@myhost.local>
2017-10-21 10:03:46 +0700

    First commit

---
+++ level-4.py
@@ -0,0 +1,18 @@
+from urllib.request import urlopen
+import re
+
+uri = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=%s"
+
+num = "12345"
+#num = str(16044/2)
+
+pattern = re.compile("and the next nothing is (\d+)")
+
+while True:
+    content = urlopen(uri % num).read().decode('utf-8')
+    match = pattern.search(content)
+    if match == None:
+        break
+    num = match.group(1)
+
+# Answer: peak.html
